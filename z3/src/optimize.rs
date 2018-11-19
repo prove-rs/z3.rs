bd5b2e20fd5e82237773bfac9350d79d120a92a6
Bruce Mitchener <bruce.mitchener@gmail.com>
2018-11-19 12:01:33 +0700

    z3: Add Optimize docs.

--- z3/src/optimize.rs
+++ z3/src/optimize.rs
@@ -8,6 +8,7 @@ use Optimize;
 use Z3_MUTEX;
 
 impl<'ctx> Optimize<'ctx> {
+    /// Create a new optimize context.
     pub fn new(ctx: &'ctx Context) -> Optimize<'ctx> {
         Optimize {
             ctx,
@@ -20,26 +21,54 @@ impl<'ctx> Optimize<'ctx> {
         }
     }
 
+    /// Assert hard constraint to the optimization context.
+    ///
+    /// # See also:
+    ///
+    /// - [`Optimize::maximize()`](#method.maximize)
+    /// - [`Optimize::minimize()`](#method.minimize)
     pub fn assert(&self, ast: &Ast<'ctx>) {
         let guard = Z3_MUTEX.lock().unwrap();
         unsafe { Z3_optimize_assert(self.ctx.z3_ctx, self.z3_opt, ast.z3_ast) };
     }
 
+    /// Add a maximization constraint.
+    ///
+    /// # See also:
+    ///
+    /// - [`Optimize::assert()`](#method.assert)
+    /// - [`Optimize::minimize()`](#method.minimize)
     pub fn maximize(&self, ast: &Ast<'ctx>) {
         let guard = Z3_MUTEX.lock().unwrap();
         unsafe { Z3_optimize_maximize(self.ctx.z3_ctx, self.z3_opt, ast.z3_ast) };
     }
 
+    /// Add a minimization constraint.
+    ///
+    /// # See also:
+    ///
+    /// - [`Optimize::assert()`](#method.assert)
+    /// - [`Optimize::maximize()`](#method.maximize)
     pub fn minimize(&self, ast: &Ast<'ctx>) {
         let guard = Z3_MUTEX.lock().unwrap();
         unsafe { Z3_optimize_minimize(self.ctx.z3_ctx, self.z3_opt, ast.z3_ast) };
     }
 
+    /// Check consistency and produce optimal values.
+    ///
+    /// # See also:
+    ///
+    /// - [`Optimize::get_model()`](#method.get_model)
     pub fn check(&self) -> bool {
         let guard = Z3_MUTEX.lock().unwrap();
         unsafe { Z3_optimize_check(self.ctx.z3_ctx, self.z3_opt) == Z3_L_TRUE }
     }
 
+    /// Retrieve the model for the last [`Optimize::check()`](#method.check)
+    ///
+    /// The error handler is invoked if a model is not available because
+    /// the commands above were not invoked for the given optimization
+    /// solver, or if the result was `Z3_L_FALSE`.
     pub fn get_model(&self) -> Model<'ctx> {
         Model::of_optimize(self)
     }
