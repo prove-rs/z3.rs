261702485fe5d740e1c407a191a0b294545a60af
Julius Rakow <julius@rakow.me>
2019-04-25 01:56:45 +0100

    use bindgen to generate bindings

---
+++ z3-sys/build.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=z3");
+
+    let bindings = bindgen::Builder::default()
+        .header("src/wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
