4765959389c39f920d9f90cb3048fc64a1bf6ef7
Bruce Mitchener <bruce.mitchener@gmail.com>
2019-06-29 00:58:03 +0700

    rustfmt

--- z3/src/symbol.rs
+++ z3/src/symbol.rs
@@ -6,9 +6,7 @@ use Symbol;
 impl Symbol {
     pub fn as_z3_symbol(&self, ctx: &Context) -> Z3_symbol {
         match self {
-            Symbol::Int(i) => {
-                unsafe { Z3_mk_int_symbol(ctx.z3_ctx, *i as ::std::os::raw::c_int) }
-            }
+            Symbol::Int(i) => unsafe { Z3_mk_int_symbol(ctx.z3_ctx, *i as ::std::os::raw::c_int) },
             Symbol::String(s) => {
                 let ss = CString::new(s.clone()).unwrap();
                 let p = ss.as_ptr();
