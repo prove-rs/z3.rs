eee38a49134570d1dc1c9cc7eececa80887f6478
Bruce Mitchener <bruce.mitchener@gmail.com>
2019-06-29 17:15:20 +0700

    Add struct docs for Context.

--- z3/src/lib.rs
+++ z3/src/lib.rs
@@ -30,13 +30,33 @@ lazy_static! {
     static ref Z3_MUTEX: Mutex<()> = Mutex::new(());
 }
 
-/// Configuration used to initialize logical contexts.
+/// Configuration used to initialize [logical contexts].
+///
+/// [logical contexts]: struct.Context.html
 pub struct Config {
     kvs: Vec<(CString, CString)>,
     z3_cfg: Z3_config,
 }
 
 /// Manager of all other Z3 objects, global configuration options, etc.
+///
+/// An application may use multiple Z3 contexts. Objects created in one context
+/// cannot be used in another one. However, several objects may be "translated" from
+/// one context to another. It is not safe to access Z3 objects from multiple threads.
+/// The only exception is the method [`interrupt()`] that can be used to interrupt a long
+/// computation.
+///
+/// # Examples:
+///
+/// Creating a context with the default configuration:
+///
+/// ```
+/// use z3::{Config, Context};
+/// let cfg = Config::new();
+/// let ctx = Context::new(&cfg);
+/// ```
+///
+/// [`interrupt()`]: #method.interrupt
 pub struct Context {
     z3_ctx: Z3_context,
 }
