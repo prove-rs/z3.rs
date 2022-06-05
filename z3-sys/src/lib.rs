41e1b2089655f094f42ce655fc3882ccd3af95b9
AndesVL <32360382+AndesVL@users.noreply.github.com>
2022-06-05 05:50:08 +0200

    fixed documentation for Z3_param_descrs related functions

--- z3-sys/src/lib.rs
+++ z3-sys/src/lib.rs
@@ -1781,7 +1781,7 @@ extern "C" {
     /// Return the number of parameters in the given parameter description set.
     pub fn Z3_param_descrs_size(c: Z3_context, p: Z3_param_descrs) -> ::std::os::raw::c_uint;
 
-    /// Return the number of parameters in the given parameter description set.
+    /// Return the name of the parameter at given index `i`.
     ///
     /// # Preconditions:
     ///
@@ -1793,6 +1793,10 @@ extern "C" {
     ) -> Z3_symbol;
 
     /// Retrieve documentation string corresponding to parameter name `s`.
+    /// 
+    /// # Preconditions:
+    /// 
+    /// - `s is a valid parameter name`
     pub fn Z3_param_descrs_get_documentation(
         c: Z3_context,
         p: Z3_param_descrs,
