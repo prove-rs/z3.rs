5fc6cab0eeb73301b349b48d1b263673411791d2
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2023-04-08 19:34:24 -0400

    implement Deref for RecFuncDecl

--- z3/src/rec_func_decl.rs
+++ z3/src/rec_func_decl.rs
@@ -3,8 +3,9 @@ use ast::Ast;
 use std::convert::TryInto;
 use std::ffi::CStr;
 use std::fmt;
+use std::ops::Deref;
 use z3_sys::*;
-use {Context, RecFuncDecl, Sort, Symbol};
+use {Context, FuncDecl, RecFuncDecl, Sort, Symbol};
 
 impl<'ctx> RecFuncDecl<'ctx> {
     pub(crate) unsafe fn wrap(ctx: &'ctx Context, z3_func_decl: Z3_func_decl) -> Self {
@@ -91,67 +92,6 @@ impl<'ctx> RecFuncDecl<'ctx> {
             );
         }
     }
-
-    /// Return the number of arguments of a function declaration.
-    ///
-    /// If the function declaration is a constant, then the arity is `0`.
-    ///
-    /// ```
-    /// # use z3::{Config, Context, RecFuncDecl, Solver, Sort, Symbol};
-    /// # let cfg = Config::new();
-    /// # let ctx = Context::new(&cfg);
-    /// let f = RecFuncDecl::new(
-    ///     &ctx,
-    ///     "f",
-    ///     &[&Sort::int(&ctx), &Sort::real(&ctx)],
-    ///     &Sort::int(&ctx));
-    /// assert_eq!(f.arity(), 2);
-    /// ```
-    pub fn arity(&self) -> usize {
-        unsafe { Z3_get_arity(self.ctx.z3_ctx, self.z3_func_decl) as usize }
-    }
-
-    /// Create a constant (if `args` has length 0) or function application (otherwise).
-    ///
-    /// Note that `args` should have the types corresponding to the `domain` of the `RecFuncDecl`.
-    pub fn apply(&self, args: &[&dyn ast::Ast<'ctx>]) -> ast::Dynamic<'ctx> {
-        assert!(args.iter().all(|s| s.get_ctx().z3_ctx == self.ctx.z3_ctx));
-
-        let args: Vec<_> = args.iter().map(|a| a.get_z3_ast()).collect();
-
-        unsafe {
-            ast::Dynamic::wrap(self.ctx, {
-                Z3_mk_app(
-                    self.ctx.z3_ctx,
-                    self.z3_func_decl,
-                    args.len().try_into().unwrap(),
-                    args.as_ptr(),
-                )
-            })
-        }
-    }
-
-    /// Return the `DeclKind` of this `RecFuncDecl`.
-    pub fn kind(&self) -> DeclKind {
-        unsafe { Z3_get_decl_kind(self.ctx.z3_ctx, self.z3_func_decl) }
-    }
-
-    /// Return the name of this `RecFuncDecl`.
-    ///
-    /// Strings will return the `Symbol`.  Ints will have a `"k!"` prepended to
-    /// the `Symbol`.
-    pub fn name(&self) -> String {
-        unsafe {
-            let z3_ctx = self.ctx.z3_ctx;
-            let symbol = Z3_get_decl_name(z3_ctx, self.z3_func_decl);
-            match Z3_get_symbol_kind(z3_ctx, symbol) {
-                SymbolKind::String => CStr::from_ptr(Z3_get_symbol_string(z3_ctx, symbol))
-                    .to_string_lossy()
-                    .into_owned(),
-                SymbolKind::Int => format!("k!{}", Z3_get_symbol_int(z3_ctx, symbol)),
-            }
-        }
-    }
 }
 
 impl<'ctx> fmt::Display for RecFuncDecl<'ctx> {
@@ -181,5 +121,13 @@ impl<'ctx> Drop for RecFuncDecl<'ctx> {
                 Z3_func_decl_to_ast(self.ctx.z3_ctx, self.z3_func_decl),
             );
         }
+    }
+}
+
+impl<'ctx> Deref for RecFuncDecl<'ctx> {
+    type Target = FuncDecl<'ctx>;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(self as *const _ as *const Self::Target) }
     }
 }
