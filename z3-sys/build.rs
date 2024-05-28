13c46f415b4007430f99f7d8197a9b4c553207a0
toolCHAINZ <mark.denhoed@cs.ox.ac.uk>
2024-05-28 20:13:11 +0100

    Update build.rs

--- z3-sys/build.rs
+++ z3-sys/build.rs
@@ -150,6 +150,17 @@ fn build_bundled_z3() {
         cfg.build_arg("-m");
         cfg.cxxflag("-DWIN32");
         cfg.cxxflag("-D_WINDOWS");
+        match env::var("PROFILE").unwrap().as_str() {
+            "debug" => {
+                cfg.define("CMAKE_MSVC_RUNTIME_LIBRARY", "MultiThreadedDebugDLL");
+            },
+            "release" => {
+                cfg.define("CMAKE_MSVC_RUNTIME_LIBRARY", "MultiThreadedDLL");
+            },
+            _ => {
+                println!("cargo:warning=Unexpected PROFILE set by cargo.");
+            }
+        }
     }
 
     let dst = cfg.build();
