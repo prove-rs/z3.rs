c00ff373093e60da217dc46f0b0a4a71e26dd007
Mark <mark.denhoed@cs.ox.ac.uk>
2025-08-18 10:00:47 +0100

    fix docs

--- z3/src/ast/rounding_mode.rs
+++ z3/src/ast/rounding_mode.rs
@@ -9,27 +9,27 @@ pub struct RoundingMode {
 }
 
 impl RoundingMode {
-    /// Create a numeral of ['RoundingMode'] sort which represents the TowardZero rounding mode.
+    /// Create a numeral of [`RoundingMode`] sort which represents the `TowardZero` rounding mode.
     pub fn round_towards_zero(ctx: &Context) -> RoundingMode {
         unsafe { Self::wrap(ctx, Z3_mk_fpa_round_toward_zero(ctx.z3_ctx.0)) }
     }
 
-    /// Create a numeral of ['RoundingMode'] sort which represents the TowardNegative rounding mode.
+    /// Create a numeral of [`RoundingMode`] sort which represents the `TowardNegative` rounding mode.
     pub fn round_towards_negative(ctx: &Context) -> RoundingMode {
         unsafe { Self::wrap(ctx, Z3_mk_fpa_round_toward_negative(ctx.z3_ctx.0)) }
     }
 
-    /// Create a numeral of ['RoundingMode'] sort which represents the TowardPositive rounding mode.
+    /// Create a numeral of [`RoundingMode`] sort which represents the `TowardPositive` rounding mode.
     pub fn round_towards_positive(ctx: &Context) -> RoundingMode {
         unsafe { Self::wrap(ctx, Z3_mk_fpa_round_toward_positive(ctx.z3_ctx.0)) }
     }
 
-    /// Create a numeral of [`RoundingMode`] sort which represents the NearestTiesToAway rounding mode.
+    /// Create a numeral of [`RoundingMode`] sort which represents the `NearestTiesToAway` rounding mode.
     pub fn round_nearest_ties_to_away(ctx: &Context) -> RoundingMode {
         unsafe { Self::wrap(ctx, Z3_mk_fpa_round_nearest_ties_to_away(ctx.z3_ctx.0)) }
     }
 
-    /// Create a numeral of [`RoundingMode`] sort which represents the NearestTiesToEven rounding mode.
+    /// Create a numeral of [`RoundingMode`] sort which represents the `NearestTiesToEven` rounding mode.
     pub fn round_nearest_ties_to_even(ctx: &Context) -> RoundingMode {
         unsafe { Self::wrap(ctx, Z3_mk_fpa_round_nearest_ties_to_even(ctx.z3_ctx.0)) }
     }
