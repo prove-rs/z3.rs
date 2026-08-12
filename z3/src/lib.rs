70081be42e669cfbacc7e1abff449f635f881f54
Ollie <75216562+Ollie-Pearce@users.noreply.github.com>
2026-08-12 11:34:05 +0100

    fix: add missing inc_ref to ApplyResult's Clone (#578)

--- z3/src/lib.rs
+++ z3/src/lib.rs
@@ -338,7 +338,7 @@ pub struct Pattern {
 }
 
 /// Collection of subgoals resulting from applying of a tactic to a goal.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct ApplyResult {
     ctx: Context,
     z3_apply_result: Z3_apply_result,
